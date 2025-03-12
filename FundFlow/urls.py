24c6e5a2c84263ba2423139f525eb63a039c6dd9
Grecia Osorio <you@example.com>
2025-03-12 11:54:53 -0500

    corrected file names and routed current pages

--- FundFlow/urls.py
+++ FundFlow/urls.py
@@ -4,11 +4,11 @@ from . import views
 urlpatterns = [
     #tester
     path('fundflow/', views.fund_flow, name='FundFlow'),
-    
-    path('', views.home, name='home'),
-    # path('signup/', views.signup_view, name='signup'),
-    # path('login/', views.login_view, name='login'),
-    # path('logout/', views.logout_view, name='logout'),
+    #path for home, which is the login page
+    path('', views.home_logIn, name='home'),
+    #path for signing up page
+    path('signup/', views.signup_view, name='signUp'),
+    #path('logout/', views.logout_view, name='logout'),
     path('landingpage/', views.landing_page, name='landingpage'),
     
 ]
