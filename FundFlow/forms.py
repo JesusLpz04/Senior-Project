08a5e09ce7d018ae5e94cb5dc34283bbe8eb266f
hannaperez <hannaperez0628@gmai.com>
2025-03-17 15:11:57 -0500

    finished user models, and user view

--- FundFlow/forms.py
+++ FundFlow/forms.py
@@ -1,14 +1,27 @@
 from django.forms import ModelForm
 from django import forms
-from .models import Poll
+from .models import Poll, CreateTicket, UserProfile
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, required=True)
+    user_type = forms.ChoiceField(
+        choices=UserProfile.USER_TYPES,
+        required=True,
+        widget=forms.RadioSelect
+    )
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2', 'user_type')
+        
 
 class CreatePollForm(ModelForm):
     class Meta: 
         model = Poll
         fields = ['question', 'option_one', 'option_two', 'option_three']
         
-        
-from .models import CreateTicket
 
 class CreateTicketForm(ModelForm):
     date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'})) 
